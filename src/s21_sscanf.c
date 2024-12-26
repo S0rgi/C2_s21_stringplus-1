38206a8a7e98cdaa2b92c7d096f33d1b38d08e44
Colen Andriod <colenand@student.21-school.ru>
2024-12-26 22:40:43 +0300

    add header for s21_sscanf

--- src/s21_sscanf.c
+++ src/s21_sscanf.c
@@ -1,32 +1,4 @@
-#include <stdarg.h>
-
-#include "s21_string.h"
-typedef struct {
-  int h, l, L;
-} Options;
-void spec_parse(const char **format, const char **str, va_list args, int width,
-                const char *nach_str, int *count, Options opt);
-void process_width(const char **format, int *width);
-void process_d(va_list args, const char **str, int width, Options opt);
-void process_i(va_list args, const char **str, int width, Options opt);
-void process_s(va_list args, const char **str, int width);
-void process_f(va_list args, const char **str, int width, Options opt);
-void process_c(va_list args, const char **str);
-void process_o(va_list args, const char **str, int width, Options opt);
-void process_x(va_list args, const char **str, int width, Options opt);
-void process_p(va_list args, const char **str);
-void process_n(va_list args, const char **str, const char *nach_str);
-void process_u(va_list args, const char **str, int width, Options opt);
-void parce_width(const char **format, Options *opt);
-void process_percent(const char **str, const char **format);
-float s21_atof(const char *str);
-void skip_width(int width, const char **ptr);
-void *get_int_ptr(va_list args, Options opt);
-void *get_unsigned_int_ptr(va_list args, Options opt);
-void *get_float_ptr(va_list args, Options opt);
-void assign_unsigned_int_result(void *uint_ptr, unsigned long long result,
-                                Options opt);
-void assign_float_result(void *float_ptr, double value, Options opt);
+#include "s21_sscanf.h"
 int s21_sscanf(const char *str, const char *format, ...) {
   va_list args;
   va_start(args, format);

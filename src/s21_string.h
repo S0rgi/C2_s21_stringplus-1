49c6e5c62e6d574daf66e93bb31567eeb74ae79f
Colen Andriod <colenand@student.21-school.ru>
2024-12-29 02:19:11 +0300

    change err list

--- src/s21_string.h
+++ src/s21_string.h
@@ -39,108 +39,52 @@ void *s21_trim(const char *src, const char *trim_chars);
 #if defined(__linux__)
 #define ERRORS                                                                 \
   {                                                                            \
-    "Undefined error: 0", "Operation not permitted",                           \
-        "No such file or directory", "No such process",                        \
-        "Interrupted system call", "Input/output error",                       \
-        "Device not configured", "Argument list too long",                     \
+    "Success", "Operation not permitted", "No such file or directory",         \
+        "No such process", "Interrupted system call", "Input/output error",    \
+        "No such device or address", "Argument list too long",                 \
         "Exec format error", "Bad file descriptor", "No child processes",      \
-        "Resource deadlock avoided", "Cannot allocate memory",                 \
+        "Resource temporarily unavailable", "Cannot allocate memory",          \
         "Permission denied", "Bad address", "Block device required",           \
-        "Device / Resource busy", "File exists", "Cross-device link",          \
-        "Operation not supported by device", "Not a directory",                \
-        "Is a directory", "Invalid argument", "Too many open files in system", \
+        "Device or resource busy", "File exists", "Invalid cross-device link", \
+        "No such device", "Not a directory", "Is a directory",                 \
+        "Invalid argument", "Too many open files in system",                   \
         "Too many open files", "Inappropriate ioctl for device",               \
         "Text file busy", "File too large", "No space left on device",         \
         "Illegal seek", "Read-only file system", "Too many links",             \
         "Broken pipe", "Numerical argument out of domain",                     \
-        "Numerical result out of range", "Resource temporarily unavailable",   \
-        "Operation now in progress", "Operation already in progress",          \
-        "Socket operation on non-socket", "Destination address required",      \
-        "Message too long", "Protocol wrong type for socket",                  \
-        "Protocol not available", "Protocol not supported",                    \
-        "Socket type not supported", "Operation not supported",                \
-        "Protocol family not supported",                                       \
-        "Address family not supported by protocol family",                     \
-        "Address already in use", "Can't assign requested address",            \
-        "Network is down", "Network is unreachable",                           \
-        "Network dropped connection on reset",                                 \
-        "Software caused connection abort", "Connection reset by peer",        \
-        "No buffer space available",                                           \
-        "Transport endpoint is already connected",                             \
-        "Transport endpoint is not connected",                                 \
-        "Can't send after transport endpoint shutdown",                        \
-        "Too many references: can't splice", "Operation timed out",            \
-        "Connection refused", "Too many levels of symbolic links",             \
-        "File name too long", "Host is down", "No route to host",              \
-        "Directory not empty", "Too many processes", "Too many users",         \
-        "Disc quota exceeded", "Stale NFS file handle",                        \
-        "Too many levels of remote in path", "RPC struct is bad",              \
-        "RPC version wrong", "RPC prog. not avail", "Program version wrong",   \
-        "Bad procedure for program", "No locks available",                     \
-        "Function not implemented", "Inappropriate file type or format",       \
-        "Authentication error", "Need authenticator", "Device power is off",   \
-        "Device error", "Value too large to be stored in data type",           \
-        "Bad executable (or shared library)", "Bad CPU type in executable",    \
-        "Shared library version mismatch", "Malformed Mach-o file",            \
-        "Operation canceled", "Identifier removed",                            \
-        "No message of desired type", "Illegal byte sequence",                 \
-        "Attribute not found", "Bad message", "EMULTIHOP (Reserved)",          \
-        "No message available on STREAM", "ENOLINK (Reserved)",                \
-        "No STREAM resources", "Not a STREAM", "Protocol error",               \
-        "STREAM ioctl timeout", "Operation not supported on socket",           \
-        "Policy not found", "State not recoverable", "Previous owner died",    \
-        "Interface output queue is full"                                       \
-  }
-
-#elif defined(__APPLE__)
-#define ERRORS                                                                 \
-  {                                                                            \
-    "Success", "Operation not permitted", "No such file or directory",         \
-        "No such process", "Interrupted system call", "I/O error",             \
-        "No such device or address", "Arg list too long", "Exec format error", \
-        "Bad file number", "No child processes", "Try again", "Out of memory", \
-        "Permission denied", "Bad address", "Block device required",           \
-        "Device or resource busy", "File exists", "Cross-device link",         \
-        "No such device", "Not a directory", "Is a directory",                 \
-        "Invalid argument", "File table overflow", "Too many open files",      \
-        "Not a typewriter", "Text file busy", "File too large",                \
-        "No space left on device", "Illegal seek", "Read-only file system",    \
-        "Too many links", "Broken pipe",                                       \
-        "Math argument out of domain of func",                                 \
-        "Math result not representable", "Resource deadlock would occur",      \
-        "File name too long", "No record locks available",                     \
+        "Numerical result out of range", "Resource deadlock avoided",          \
+        "File name too long", "No locks available",                            \
         "Function not implemented", "Directory not empty",                     \
-        "Too many symbolic links encountered", "Operation would block",        \
+        "Too many levels of symbolic links", "Unknown error 41",               \
         "No message of desired type", "Identifier removed",                    \
         "Channel number out of range", "Level 2 not synchronized",             \
         "Level 3 halted", "Level 3 reset", "Link number out of range",         \
         "Protocol driver not attached", "No CSI structure available",          \
         "Level 2 halted", "Invalid exchange", "Invalid request descriptor",    \
         "Exchange full", "No anode", "Invalid request code", "Invalid slot",   \
-        "Resource deadlock would occur", "Bad font file format",               \
-        "Device not a stream", "No data available", "Timer expired",           \
-        "Out of streams resources", "Machine is not on the network",           \
-        "Package not installed", "Object is remote", "Link has been severed",  \
-        "Advertise error", "Srmount error", "Communication error on send",     \
-        "Protocol error", "Multihop attempted", "RFS specific error",          \
-        "Not a data message", "Value too large for defined data type",         \
-        "Name not unique on network", "File descriptor in bad state",          \
-        "Remote address changed", "Can not access a needed shared library",    \
+        "Unknown error 58", "Bad font file format", "Device not a stream",     \
+        "No data available", "Timer expired", "Out of streams resources",      \
+        "Machine is not on the network", "Package not installed",              \
+        "Object is remote", "Link has been severed", "Advertise error",        \
+        "Srmount error", "Communication error on send", "Protocol error",      \
+        "Multihop attempted", "RFS specific error", "Bad message",             \
+        "Value too large for defined data type", "Name not unique on network", \
+        "File descriptor in bad state", "Remote address changed",              \
+        "Can not access a needed shared library",                              \
         "Accessing a corrupted shared library",                                \
         ".lib section in a.out corrupted",                                     \
         "Attempting to link in too many shared libraries",                     \
-        "Cannot exec a shared library directly", "Illegal byte sequence",      \
+        "Cannot exec a shared library directly",                               \
+        "Invalid or incomplete multibyte or wide character",                   \
         "Interrupted system call should be restarted", "Streams pipe error",   \
         "Too many users", "Socket operation on non-socket",                    \
         "Destination address required", "Message too long",                    \
         "Protocol wrong type for socket", "Protocol not available",            \
         "Protocol not supported", "Socket type not supported",                 \
-        "Operation not supported on transport endpoint",                       \
-        "Protocol family not supported",                                       \
+        "Operation not supported", "Protocol family not supported",            \
         "Address family not supported by protocol", "Address already in use",  \
         "Cannot assign requested address", "Network is down",                  \
-        "Network is unreachable",                                              \
-        "Network dropped connection because of reset",                         \
+        "Network is unreachable", "Network dropped connection on reset",       \
         "Software caused connection abort", "Connection reset by peer",        \
         "No buffer space available",                                           \
         "Transport endpoint is already connected",                             \
@@ -149,9 +93,9 @@ void *s21_trim(const char *src, const char *trim_chars);
         "Too many references: cannot splice", "Connection timed out",          \
         "Connection refused", "Host is down", "No route to host",              \
         "Operation already in progress", "Operation now in progress",          \
-        "Stale NFS file handle", "Structure needs cleaning",                   \
+        "Stale file handle", "Structure needs cleaning",                       \
         "Not a XENIX named type file", "No XENIX semaphores available",        \
-        "Is a named type file", "Remote I/O error", "Quota exceeded",          \
+        "Is a named type file", "Remote I/O error", "Disk quota exceeded",     \
         "No medium found", "Wrong medium type", "Operation canceled",          \
         "Required key not available", "Key has expired",                       \
         "Key has been revoked", "Key was rejected by service", "Owner died",   \
